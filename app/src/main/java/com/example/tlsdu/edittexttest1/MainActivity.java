e781a17aaafdf34ca7b1f5730ffd7499222829aa
sinyoung550 <sinyoung550@gamil.com>
2017-03-27 19:11:06 +0900

    #2 Java Event complete

--- app/src/main/java/com/example/tlsdu/edittexttest1/MainActivity.java
+++ app/src/main/java/com/example/tlsdu/edittexttest1/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.tlsdu.edittexttest1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edit1;
+    TextView textResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edit1=(EditText)findViewById(R.id.edit1); //강제 형변환 (EditText)<<이거
+        Button butOk=(Button)findViewById(R.id.but_ok);
+        textResult=(TextView)findViewById(R.id.text_result);
+
+        butOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name=edit1.getText().toString();
+                textResult.setText("이번우승"+name+"입니다.");
+            }
+        });
     }
 }
